e58f8935b123c9a63b2e05df4cfacb5e7039b39f
Jia Li <jijli@cs.stonybrook.edu>
2016-08-30 07:46:36 -0700

    Edited test.c

--- hw0/test.c
+++ hw0/test.c
@@ -1,7 +1,9 @@
 /* Hello Students! program */
+
 #include <stdlib.h>
 #include <stdio.h>
+
 int main(int argc, char* argv[]) {
-fprintf(stdout, "%s\n", "Hello Students!");
-return EXIT_SUCCESS;
+	fprintf(stdout, "%s\n", "Hello Students!");
+	return EXIT_SUCCESS;
 }
