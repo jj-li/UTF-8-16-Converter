f541de266b8c143e488599ac9479d51a655a3ffc
Jia Li <jijli@cs.stonybrook.edu>
2016-09-17 22:37:06 -0400

    Working on Part2, making the program work on Sparky.

---
+++ hw2/.bash_history
@@ -0,0 +1,94 @@
+pico .login
+exit
+exit
+pico .login
+pwd
+pico tickets.pl
+ls
+more tickets.pl
+ls -l
+chmod +x tickets.pl
+ls -l
+who | wc -l
+chmod 755 tickets.pl
+ls -l
+perl tickets.pl title=deadpool quantity=4
+cd www
+cd cgi-bin
+ls
+pico ica4.pl
+ls
+chmod 775 ica4.pl
+ls
+ls -l
+ls-l
+ls -l
+chmod u+x ica4.pl
+ls -l
+perl ica4.pl bgcolor=tomato num=45
+pico ica4.pl
+pico ica4.pl
+perl ica4.pl bgcolor=tomato num=45
+ls -l
+cd www
+cd cgi-bin
+ls
+chmod 755 ica4.pl
+ls -l
+exit
+ls
+cd www
+ls
+cd hw5
+ls
+pico burger.cgi
+pico burger.cgi
+ls
+mkdir cgi-bin
+ls
+move burger.cgi cgi-bin
+mv burger.cgi cgi-bin
+ls
+cd cgi-bin
+ls
+mv burger.cgi burger.pl
+ls
+mv burger.pl ./
+mv burger.pl ../
+ls
+cd ./
+ls
+cd ../
+ls
+mv burger.pl cgi-bin
+cd
+ls
+cd www
+ls
+cd cgi-bin
+ls
+cd 
+cd www
+cd hw5
+ls
+cd cgi-bin
+pico burger.pl
+ls
+cd
+cd www
+ls
+more ica5.html
+ls
+cd hw5/cgi-bin
+mv burger.pl ../
+ls
+cd ../
+ls
+cd
+cd www/cgi-bin
+ls
+chmod +755 burger.pl
+chmod 755 burger.pl
+ls
+ls -l
+pico burger.pl
--- hw2/src/utfconverter.c
+++ hw2/src/utfconverter.c
@@ -3,40 +3,53 @@
 char* filename;
 endianness source;
 endianness conversion;
+int sparky;
 int main(int argc, char** argv)
 {
+	int fd;
+	int rv;
+	Glyph* glyph;
+	unsigned int buf[2]; 
+
 	/*After calling parse_args(), filename and conversion should be set. */
 	parse_args(argc, argv);
 
-	int fd = open(filename, O_RDONLY); 
-	unsigned int buf[2]; 
-	int rv = 0; 
+	fd = open(filename, O_RDONLY); 
+	rv = 0; 
 	memset(buf, 0, sizeof(buf));
-	Glyph* glyph = malloc(sizeof(Glyph)+1); 
-	
+	glyph = malloc(sizeof(Glyph)+1); 
+	sparky = 0;
 	/*Handle BOM bytes for UTF16 specially. 
     Read our values into the first and second elements.*/
 	if((rv = read(fd, &buf[0], 1)) == 1 && 
-			(rv = read(fd, &buf[1], 1)) == 1){ 
+			(rv = read(fd, &buf[1], 1)) == 1){
+		void* memset_return; 
+
 		if(buf[0] == 0xff && buf[1] == 0xfe){
 			/*file is little endian*/
 			source = LITTLE; 
 		} else if(buf[0] == 0xfe && buf[1] == 0xff){
 			/*file is big endian*/
 			source = BIG;
+		} else if (buf[0] == 0xff000000 && buf[1] == 0xfe000000){
+			source = LITTLE;
+			sparky = 1;
+		} else if (buf[0] == 0xfe000000 && buf[1] == 0xff000000){
+			source = BIG;
+			sparky = 1;
 		} else {
 			/*file has no BOM*/
 			free(&glyph->bytes); 
 			fprintf(stderr, "File has no BOM.\n");
 			quit_converter(NO_FD); 
 		}
-		void* memset_return = memset(glyph, 0, sizeof(Glyph)+1);
+		memset_return = memset(glyph, 0, sizeof(Glyph)+1);
 		/* Memory write failed, recover from it: */
 		if(memset_return == NULL){
 			/* tweak write permission on heap memory. */
-			asm("movl $8, %esi\n\t"
+			/*asm("movl $8, %esi\n\t"
 			    "movl $.LC0, %edi\n\t"
-			    "movl $0, %eax");
+			    "movl $0, %eax");*/
 			/* Now make the request again. */
 			memset(glyph, 0, sizeof(Glyph)+1);
 		}
@@ -57,14 +70,16 @@ int main(int argc, char** argv)
 	/* Now deal with the rest of the bytes.*/
 	while((rv = read(fd, &buf[0], 1)) == 1 &&  
 			(rv = read(fd, &buf[1], 1)) == 1){
+		void* memset_return;
+
 		write_glyph(fill_glyph(glyph, buf, source, &fd));
-		void* memset_return = memset(glyph, 0, sizeof(Glyph)+1);
+		memset_return = memset(glyph, 0, sizeof(Glyph)+1);
 	        /* Memory write failed, recover from it: */
 	        if(memset_return == NULL){
 		        /* tweak write permission on heap memory. */
-		        asm("movl $8, %esi\n\t"
+		        /*asm("movl $8, %esi\n\t"
 		            "movl $.LC0, %edi\n\t"
-		            "movl $0, %eax");
+		            "movl $0, %eax");*/
 		        /* Now make the request again. */
 		        memset(glyph, 0, sizeof(Glyph)+1);
 	        }
@@ -92,10 +107,17 @@ Glyph* swap_endianness(Glyph* glyph)
 
 Glyph* fill_glyph(Glyph* glyph, unsigned int data[2], endianness end, int* fd)
 {
+	unsigned int bits;
+
+	if (sparky == 1) {
+		data[0] = data[0] >> 24;
+		data[1] = data[1] >> 24;
+	}
 	glyph->bytes[0] = data[0];
 	glyph->bytes[1] = data[1];
 
-	unsigned int bits = 0; 
+	
+	bits = 0; 
 	if (end == BIG) {
 		bits = bits | (data[0] + (data[1] << 8));
 	}
@@ -119,10 +141,9 @@ Glyph* fill_glyph(Glyph* glyph, unsigned int data[2], endianness end, int* fd)
 					print_help();//LONE SURROGATE PAIR! ERROR!
 			}
 	}*/
-	//utf16BE-special.txt
 	if(bits > 0x000F && bits < 0xF8FF){ 
- 			//bits = '0'; bits |= (bytes[FIRST] + (bytes[SECOND] << 8))
- 			if(bits > 0xDAAF && bits < 0x00FF){ //Check low surrogate pair.
+ 			/*bits = '0'; bits |= (bytes[FIRST] + (bytes[SECOND] << 8))*/
+ 			if(bits > 0xDAAF && bits < 0x00FF){ /*Check low surrogate pair.*/
  				glyph->surrogate = false; 
  			} else {
  				lseek(*fd, 0, SEEK_CUR); 
@@ -183,7 +204,7 @@ void parse_args(int argc, char** argv)
 	optind = optind + 1;
 	if(optind < argc){
 		filename = strdup(argv[optind]);
-		//strcpy(filename, argv[optind]);
+		/*strcpy(filename, argv[optind]);*/
 	} else {
 		fprintf(stderr, "Filename not given.\n");
 		print_help();
